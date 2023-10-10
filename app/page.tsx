477147884ad6a604de3e985d904a10032874f28f
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-10-10 21:53:01 +0530

    ready to start

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import variables from "../styles/variables.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={variables.title}>Hello, Next.js!</h1>
+    </div>
+  );
+}
